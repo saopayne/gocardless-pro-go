ccc6b9731a424767a9154efa5606c283918a9825
Oyewale Ademola <saopayne@gmail.com>
2017-08-26 20:41:33 +0100

    create subscription request

--- subscription.go
+++ subscription.go
@@ -1,5 +1,9 @@
 package gocardless_pro_go
 
+import (
+	"fmt"
+	"time"
+)
 
 type SubscriptionService service
 
@@ -34,6 +38,124 @@ type Month	struct {
 type UpcomingPayment struct {
 	Amount		int			`json:"amount,omitempty"`
 	ChargeDate	string		`json:"charge_date,omitempty"`
+}
+
+
+type SubscriptionListRequest struct {
+	CreatedAt 			CreatedAt	`json:"created_at,omitempty"`
+	Limit				int			`json:"limit,omitempty"`
+	Before				string		`json:"before,omitempty"`
+	After				string		`json:"after,omitempty"`
+	Customer			string		`json:"customer,omitempty"`
+	Mandate				string		`json:"mandate,omitempty"`
+}
+
+type SubscriptionList struct {
+	Meta   ListMeta
+	Values []Subscription `json:"data"`
+}
+
+type SubscriptionCreateRequest struct {
+	Metadata			map[string]string		`json:"metadata,omitempty"`
+	PaymentReference	string					`json:"payment_reference,omitempty"`
+	Links				[]string				`json:"links,omitempty"`
+	StartDate			string					`json:"start_date,omitempty"`
+	Name				string					`json:"name,omitempty"`
+	Month				string					`json:"month,omitempty"`
+	Interval			int						`json:"interval,omitempty"`
+	EndDate				string					`json:"end_date,omitempty"`
+	DayOfMonth			string					`json:"day_of_month,omitempty"`
+	IntervalUnit		IntervalUnit			`json:"interval_unit,omitempty"`
+	Amount				string					`json:"amount,omitempty"`
+	Currency			string					`json:"currency,omitempty"`
+	AppFee				string					`json:"app_fee,omitempty"`
+	Count				string					`json:"count,omitempty"`
+	Scheme				string					`json:"scheme,omitempty"`
+	AccountNumber		string					`json:"account_number,omitempty"`
+
+}
+
+type SubscriptionCancelRequest struct {
+	Identity 	string  	`json:"identity,omitempty"`
+}
+
+
+// Create creates a new subscription
+func (s *SubscriptionService) CreateSubscription(subscriptionReq *SubscriptionCreateRequest) (*Subscription, error) {
+	u := fmt.Sprintf("/subscriptions")
+	subscription := &Subscription{}
+	err := s.client.Call("POST", u, subscriptionReq, subscription)
+
+	return subscription, err
+}
+
+// List returns a list of payments
+func (s *PaymentService) ListPayments(req *PaymentListRequest) (*PaymentList, error) {
+	return s.ListNPayments(10, 0, req)
+}
+
+func (s *PaymentService) ListNPayments(count, offset int, req *PaymentListRequest) (*PaymentList, error) {
+	params := url.Values{}
+	params.Add("after", req.After)
+	params.Add("before", req.Before)
+	params.Add("created_at[gt]", req.CreatedAt.Gt)
+	params.Add("created_at[gte]", req.CreatedAt.Gte)
+	params.Add("created_at[lt]", req.CreatedAt.Lt)
+	params.Add("created_at[lte]", req.CreatedAt.Lte)
+	params.Add("limit", string(req.Limit))
+	params.Add("status", req.Status)
+	params.Add("mandate", req.Mandate)
+	params.Add("customer", req.Customer)
+	params.Add("creditor", req.Creditor)
+	params.Add("currency", req.Currency)
+	params.Add("subscription", req.Subscription)
+
+	u := paginateURL("/payments", count, offset)
+	payments := &PaymentList{}
+	err := s.client.Call("GET", u, params, payments)
+
+	return payments, err
+}
+
+
+func (s *PaymentService) GetPayment(id string) (*Payment, error) {
+	u := fmt.Sprintf("/payments/%s", id)
+	payment := &Payment{}
+	err := s.client.Call("GET", u, nil, payment)
+
+	return payment, err
+}
+
+
+func (s *PaymentService) UpdatePayment(updatedPayment *Payment, metadata map[string]string) (*Payment, error) {
+	params := url.Values{}
+	params.Add("metadata", string(metadata))
+	u := fmt.Sprintf("/payments/%d", updatedPayment.ID)
+	payment := &Payment{}
+	err := s.client.Call("PUT", u, params, payment)
+
+	return payment, err
+}
+
+
+func (s *PaymentService) CancelPayment(paymentToCancel *Payment, metadata map[string]string) (*Response, error) {
+	params := url.Values{}
+	params.Add("metadata", string(metadata))
+	u := fmt.Sprintf("/payments/%s/actions/cancel", paymentToCancel.ID)
+	resp := &Response{}
+	err := s.client.Call("POST", u, params, resp)
+
+	return resp, err
+}
+
+func (s *MandateService) RetryPayment(payment *Payment, metadata map[string]string) (*Response, error) {
+	params := url.Values{}
+	params.Add("metadata", string(metadata))
+	u := fmt.Sprintf("/payments/%s/actions/retry", payment.ID)
+	resp := &Response{}
+	err := s.client.Call("POST", u, params, resp)
+
+	return resp, err
 }
 
 
