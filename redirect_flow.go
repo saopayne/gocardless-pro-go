467e7fec68fac8ee90bace86adb8cda30e82670b
Oyewale Ademola <saopayne@gmail.com>
2017-08-26 20:08:20 +0100

    Add create and get single redirect flow to RedirectFlow service

--- redirect_flow.go
+++ redirect_flow.go
@@ -1,5 +1,6 @@
 package gocardless_pro_go
 
+import "fmt"
 
 type RedirectFlowService service
 
@@ -13,3 +14,51 @@ type RedirectFlow struct {
 	SuccessRedirectUrl			string				`json:"success_redirect_url,omitempty"`
 	Links						[]MandateLink		`json:"links,omitempty"`
 }
+
+type RedirectFlowCreateRequest struct {
+	PrefilledCustomer			PrefilledCustomer	`json:"prefilled_customer,omitempty"`
+	Description					string				`json:"description,omitempty"`
+	Scheme						string				`json:"scheme,omitempty"`
+	RedirectUrl					string				`json:"redirect_url,omitempty"`
+	SessionToken				string				`json:"session_token,omitempty"`
+	SuccessRedirectUrl			string				`json:"success_redirect_url,omitempty"`
+	Links						[]string			`json:"links,omitempty"`
+}
+
+type PrefilledCustomer struct {
+	ID             			int            		`json:"id,omitempty"`
+	CreatedAt      			string         		`json:"createdAt,omitempty"`
+	UpdatedAt      			string         		`json:"updatedAt,omitempty"`
+	AddressLine1			string				`json:"address_line1,omitempty"`
+	AddressLine2			string				`json:"address_line2,omitempty"`
+	AddressLine3			string				`json:"address_line3,omitempty"`
+	CompanyName				string				`json:"company_name,omitempty"`
+	CountryCode				string				`json:"country_code,omitempty"`
+	Email          			string         		`json:"email,omitempty"`
+	FamilyName      		string         		`json:"family_name,omitempty"`
+	GivenName       		string         		`json:"given_name,omitempty"`
+	City					string				`json:"city,omitempty"`
+	Language				string				`json:"language,omitempty"`
+	PostalCode				string				`json:"postal_code,omitempty"`
+	Region					string				`json:"region,omitempty"`
+	SwedishIdentityNumber	string				`json:"swedish_identity_number,omitempty"`
+	Metadata       			map[string]string   `json:"metadata,omitempty"`
+}
+
+
+func (s *RedirectFlowService) Create(redirectFlow *RedirectFlow) (*RedirectFlow, error) {
+	u := fmt.Sprintf("/redirect_flows")
+	rFlow := &RedirectFlow{}
+	err := s.client.Call("POST", u, redirectFlow, rFlow)
+
+	return rFlow, err
+}
+
+func (s *RedirectFlowService) GetRedirectFlow(id string) (*RedirectFlow, error) {
+	u := fmt.Sprintf("/redirect_flows/%s", id)
+	rFlow := &RedirectFlow{}
+	err := s.client.Call("GET", u, nil, rFlow)
+
+	return rFlow, err
+}
+
